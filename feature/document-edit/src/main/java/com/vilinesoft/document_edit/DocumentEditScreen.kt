099817aac633d373c969d855af87aaf470656bf9
Oleksandr <karvinokk@gmail.com>
2023-10-20 22:13:47 +0300

    task-12. update/add item to document dialogs & logic (not finished) (wip)

--- feature/document-edit/src/main/java/com/vilinesoft/document_edit/DocumentEditScreen.kt
+++ feature/document-edit/src/main/java/com/vilinesoft/document_edit/DocumentEditScreen.kt
@@ -20,29 +20,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.vilinesoft.document_edit.DocumentEditContract.*
 import com.vilinesoft.domain.model.DocumentItem
 import com.vilinesoft.domain.model.mockedDocuments
-import com.vilinesoft.domain.util.takeIfDouble
 import com.vilinesoft.domain.util.toDoubleString
-import com.vilinesoft.ui.components.Button
-import com.vilinesoft.ui.components.TextField
 import com.vilinesoft.ui.components.TopBarTitle
 import com.vilinesoft.ui.components.keyEventHandler
 import com.vilinesoft.ui.theme.DefaultShape
 import com.vilinesoft.ui.theme.FontSize
-import com.vilinesoft.ui.theme.LargeShape
 import com.vilinesoft.ui.theme.PereoblikTheme
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.collectLatest
@@ -93,11 +84,11 @@ fun DocumentEditContent(
             contentAlignment = Alignment.TopCenter,
             modifier = Modifier.weight(1f)
         ) {
-            state.document?.items?.let {
+            state.document?.items?.let { items ->
                 ItemsList(
-                    documents = it,
-                    onItemClicked = { onIntent(UIIntent.ClickItem) },
-                    onItemLongClicked = { onIntent(UIIntent.ClickItem) }
+                    documents = items,
+                    onItemClicked = { onIntent(UIIntent.ItemClick(it)) },
+                    onItemLongClicked = { onIntent(UIIntent.ItemLongClick(it)) }
                 )
             }
             androidx.compose.animation.AnimatedVisibility(
@@ -127,63 +118,11 @@ fun DocumentEditContent(
             onDismissRequest = { onIntent(it) }
         )
     }
-}
-
-@Composable
-fun ItemCreationDialog(
-    onConfirmClick: (UIIntent.CreateItemConfirmClick) -> Unit,
-    onDismissRequest: (UIIntent.DismissCreationDialog) -> Unit
-) {
-    val nameState = rememberSaveable { mutableStateOf("") }
-    val priceState = rememberSaveable { mutableStateOf("") }
-    val qtyState = rememberSaveable { mutableStateOf("1") }
-
-    Dialog(onDismissRequest = { onDismissRequest(UIIntent.DismissCreationDialog) }) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = MaterialTheme.colorScheme.background,
-                    shape = LargeShape
-                )
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Text(text = "Новий товар")
-            TextField(
-                titleString = "Назва товару",
-                value = nameState.value,
-                onValueChange = { nameState.value = it.text }
-            )
-            TextField(
-                titleString = "Ціна",
-                value = priceState.value,
-                keyboardType = KeyboardType.Decimal,
-                onValueChange = {
-                    priceState.value = it.text.takeIfDouble()?: priceState.value
-                }
-            )
-            TextField(
-                titleString = "Кількість",
-                value = qtyState.value,
-                keyboardType = KeyboardType.Decimal,
-                onValueChange = {
-                    qtyState.value = it.text.takeIfDouble()?: qtyState.value
-                }
-            )
-            Button(
-                text = "Створити",
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .fillMaxWidth()
-            ) {
-                onConfirmClick(UIIntent.CreateItemConfirmClick(
-                    name = nameState.value,
-                    price = priceState.value,
-                    qty = qtyState.value
-                ))
-            }
-        }
+    state.dialogUpdateItemState?.let {
+        ItemUpdateDialog(
+            state = it,
+            onIntent = onIntent,
+        )
     }
 }
 
@@ -254,7 +193,6 @@ fun Item(
     }
 }
 
-
 @Preview
 @Composable
 fun DocumentEditPreview() {
@@ -263,7 +201,16 @@ fun DocumentEditPreview() {
             state = UIState(
                 document = mockedDocuments()[0],
                 barcode = "123",
-                dialogItemCreationVisible = true
+                dialogItemCreationVisible = false,
+                dialogUpdateItemState = UpdateItemDialogState(
+                    itemName = "Name",
+                    isNameEditable = false,
+                    qtyFact = 1.0,
+                    qtyBalance = 1.0,
+                    count = 1.0,
+                    price = 1.0,
+                    unitType = "шт"
+                )
             ),
             modifier = Modifier,
             onIntent = {}
